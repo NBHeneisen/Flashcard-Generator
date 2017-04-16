38e7e50d507b10760c539c84c72c1e24cb6b7acc
Nicholas Heneisen <Nicholas Heneisen>
2017-04-15 21:44:32 -0400

    Back to basics. Inquirer took too much time to implement and been without computer a couple days this week. Basic functionality completed

--- constructors.js
+++ constructors.js
@@ -1,22 +1,26 @@
-inquirer = require("inquirer")
-
+//constructor that creates a basic Q&A flashcard
 function BasicCard (front, back) {
-    this.front = front,
-    this.back = back,
+    //question that appears on the front of the flash card
+    this.front = front;
+    //answer that appears on the back of the flash card
+    this.back = back;
 };
 
+//constructor that creates a flashcard with a cloze
 function ClozeCard (text, cloze) {
-    //deletedText= return deleted text
-    this.cloze=cloze,
-    //partialText= returns only the partial text that was not deleted
-    this.partial= text.replace(this.cloze, "..."),
-    //fullText= returns the full text
-    this.text=text,
-    //error= console log error if cloze deletion does NOT appear in the input text
-    this.error= function() {
-        if this.text.includes(this.cloze) {
-            console.log("The cloze does not appear in the full text")
-        };
-    },
+    //cloze= return deleted text
+    this.cloze=cloze;
+    //text= returns the full text
+    this.text=text;
 };
 
+//partial= returns only the partial text that was not deleted
+ClozeCard.prototype.partial =  function() {
+    if (this.text.includes(this.cloze)) {
+        console.log(this.text.replace(this.cloze, "..."));
+        return;
+    } else {
+        console.log("The cloze does not appear in the full text");
+        return;
+    }
+};
